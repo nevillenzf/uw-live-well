d24ccb7b86648d28b14a138cf3d203ad5788103f
nevillenzf <nzng@wisc.edu>
2019-10-03 17:02:45 -0500

    Updated serverside functions and database functions

--- server/app.py
+++ server/app.py
@@ -1,15 +1,58 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 import os
+import datetime
+import json
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+from models import User
+
 @app.route('/')
 def index():
-    return 'This is the web server with the PostgreSQL shit'
+    return 'This is the web server with the PostgreSQL database shit'
+
+#User related methods go here may branch out into its own file in the future
+@app.route('/user', methods=['GET','POST'])
+def user():
+    #Get user information
+    if request.method == 'GET':
+        data = request.get_json(force=True)
+        print(data)
+        query = User.query.filter_by(name=data["name"])
+        print(query)
+        
+        return "testing", 200
+    #Create new user OR logging in
+    if request.method == 'POST':
+        data = request.get_json(force=True);
+        query = db.execute("SELECT * FROM users WHERE name={};".format(data.name))
+        print(query)
+        return "okay", 200
+
+@app.route('/register', methods=['POST'])
+def register_acc():
+    #Receives a json file with User information including
+    #Create new user if it does not exist else returns an error message
+    if request.method == 'POST':
+        print("Attempting to create new user")
+        return "poop", 200
+
+@app.route('/fblogin', methods=['POST'])
+def fb_login():
+    #Receives a json file with User information including
+    #Create new user if it does not exist else returns an error message
+    data = request.get_json(force=True);
+    if request.method == 'POST':
+        #Check if user exist in database
+        query = db.execute("SELECT * FROM users WHERE fbID={} AND fbAccessToken={};"
+                    .format(data.fbID,data.fbAccessToken))
+        print(query)
+        return "poop", 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ server/models.py
@@ -0,0 +1,42 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    #If logging in through Facebook, DOES NOT require password
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String())
+    pic_url = db.Column(db.String()) #this is going to hold a URL
+    email = db.Column(db.String(), nullable=False)
+    fbAccessToken = db.Column(db.String(), nullable=True)
+    fbID = db.Column(db.Numeric, nullable=True)
+
+    #In the future initialize pic_url with None but when pulled back to React, just full from app storage
+    def __init__(self, name, email, password=None, pic_url=None, fbAccessToken=None, fbID=None):
+        self.name = name
+        self.email = email
+        if fbAccessToken is not None:
+            self.fbAccessToken = fbAccessToken
+            self.fbID = fbID
+            self.password = None #Regardless of what's passed into the parameters
+            self.pic_url= pic_url
+        else:
+            self.fbAccessToken = None
+            self.fbID = None
+            self.password = password
+            self.pic_url = pic_url
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'password': self.password,
+            'pic_url':self.pic_url,
+            'email': self.email,
+            'fbAccessToken': self.fbAccessToken,
+            'fbID': self.fbID,
+        }
